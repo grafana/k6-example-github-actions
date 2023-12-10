5aa887f4bb095ca4638dae509cd24b82e77e51d8
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-12-10 15:40:58 +0100

    Fix `cloud` workflow by setting `projectID`

--- cloud/script.js
+++ cloud/script.js
@@ -4,7 +4,13 @@ import http from "k6/http";
 export let options = {
     stages: [
         { duration: "20s", target: 10 }
-    ]
+    ],
+    ext: {
+        loadimpact: {
+          name: 'k6-example-github-actions',
+          projectID: 3673231,
+        },
+      },
 };
 
 export default function() {
